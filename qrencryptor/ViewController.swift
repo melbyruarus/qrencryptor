815947c0cea01f29a42d77aaa1cbeff3c388e2ee
Melby Ruarus <melby@ruarus.co.nz>
2020-04-13 14:53:00 +1000

    Initial Commit

---
+++ qrencryptor/ViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ViewController.swift
+//  qrencryptor
+//
+//  Created by Melby Ruarus on 13/4/20.
+//  Copyright © 2020 Melby Ruarus. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+}
+
